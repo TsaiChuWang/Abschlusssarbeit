940d05b241d5ed3dcc21913d55acc590db536896
TsaiChuWang <wtsaichu@gmail.com>
2024-09-29 10:00:04 +0800

    CPLEX Failed, Sedumi and python fit

--- Robust_Optimization/Quelle/Haupt_SEDUMI.m
+++ Robust_Optimization/Quelle/Haupt_SEDUMI.m
@@ -14,8 +14,9 @@ P = 3
 E = 4
 W = 3
 C = 60
+N = 5
 
-% f_st = sdpvar(N, N, E); 
+f_st = sdpvar(N, N, E); 
 x = sdpvar(P); 
 y = sdpvar(W, E); 
 % f_st = sdpvar(N, N, E); 
@@ -24,9 +25,11 @@ alpha = sdpvar(1);
 Objective = alpha;
 Constraints = [];
 
+% Constraints = [Constraints, f_st(0, 0) == mu(p)];
 mu = [10.365820, 9.65275435, 8.4787278]
 
 for p = 1:P
+	% disp(p)
 	Constraints = [Constraints, x(p) == mu(p)];
 end
 
@@ -42,10 +45,10 @@ for w=1:W
 	end
 end
 
-Constraints = [Constraints, 2.145966026289347*norm((y(0,0)*(3.1658205/10.3658205)), 2) <= alpha*C - y(0,0)]
-Constraints = [Constraints, 2.145966026289347*norm((y(1,1)*(3.6270764/9.65275)), 2) <= alpha*C - y(1,1)]
-Constraints = [Constraints, 2.145966026289347*norm((y(2,2)*(3.6193458/8.478727)), 2) <= alpha*C - y(2,2)]
-Constraints = [Constraints, 2.145966026289347*norm((y(0,3)*(3.1658205/10.3658205))+(y(1,3)*(3.6270764/9.65275))+(y(2,3)*(3.6193458/8.478727)), 2) <= alpha*C - (y(0,3)+y(1,3)+y(2,3))]
+Constraints = [Constraints, 2.145966026289347*norm((y(1,1)*(3.1658205/10.3658205)), 2) <= alpha*C - y(1,1)]
+Constraints = [Constraints, 2.145966026289347*norm((y(2,2)*(3.6270764/9.65275)), 2) <= alpha*C - y(2,2)]
+Constraints = [Constraints, 2.145966026289347*norm((y(3,3)*(3.6193458/8.478727)), 2) <= alpha*C - y(3,3)]
+Constraints = [Constraints, 2.145966026289347*norm((y(1,4)*(3.1658205/10.3658205))+(y(2,4)*(3.6270764/9.65275))+(y(3,4)*(3.6193458/8.478727)), 2) <= alpha*C - (y(1,4)+y(2,4)+y(3,4))]
 
 
 Constraints = [Constraints, alpha >= 0];

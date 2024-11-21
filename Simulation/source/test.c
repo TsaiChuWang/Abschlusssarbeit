9201105c98a4d1518ba85338c834d117af328ed6
TsaiChuWang <wtsaichu@gmail.com>
2024-11-22 00:31:43 +0800

    [Version 2] Update configuration with unit

--- Simulation/source/test.c
+++ Simulation/source/test.c
@@ -19,7 +19,8 @@
 
 #define GENERATE_TRAFFIC 1
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
     char command[MAX_COMMAND_LENGTH];
     // if(atoi(argv[1]) == GENERATE_TRAFFIC){
@@ -31,16 +32,16 @@ int main(int argc, char *argv[]){
     //     system("mkdir ../data/"NAME"/timestamp/images");
     // }
 
-
     // configuration.h
     configuration config;
 
-    if (ini_parse(CONFIGURATION_PATH, handler, &config) < 0) {
+    if (ini_parse(CONFIGURATION_PATH, handler, &config) < 0)
+    {
         printf("Can't load configuration \"%s\"\n", CONFIGURATION_PATH);
         return EXIT_FAILURE;
     }
 
-    show_configuration(config);
+    // show_configuration(config);
 
     // config.packet_size = 512;
     // modify_ini_file(CONFIGURATION_PATH, &config);
@@ -49,82 +50,95 @@ int main(int argc, char *argv[]){
     reduction_inif_file(CONFIGURATION_PATH);
 #endif
 
+    long unit;
+    switch (config.unit)
+    {
+    case UNIT_MBPS:
+        unit = MBPS;
+        break;
+    case UNIT_KBPS:
+        unit = KBPS;
+        break;
+    default:
+        unit = MBPS;
+    }
+
     // Clean
     sprintf(command, "rm -r %s", config.data_path);
     system(command);
     sprintf(command, "mkdir %s", config.data_path);
     system(command);
-    
+
     // capacity.py
-    sprintf(command, "python3 "PYTHON_CAPACITY_CALCULATION_PATH" %s %d", CONFIGURATION_PATH, 0);
+    sprintf(command, "python3 " PYTHON_CAPACITY_CALCULATION_PATH " %s %d", CONFIGURATION_PATH, 0);
     system(command);
-  
+
     double capacity = obtain_capacity();
-    printf("capacity : %f\n", capacity);
+    printf("capacity : %f bps\n", capacity * unit);
 
-//     // Initialize tenants 
-//     tenant* tenants = initialize_tenants(config.tenant_number, config.simulation_time);
+    //     // Initialize tenants
+    //     tenant* tenants = initialize_tenants(config.tenant_number, config.simulation_time);
 
-//     // Decide traffic mode
-//     for(int index = 0; index < config.tenant_number; index++){
-//         tenant t = (tenant)*(tenants+index);
-//         switch(config.traffic_mode){
-//             case TRAFFIC_MODE_INTERVAL:
-//                 tenants[index].traffic_mode = TENANT_TRAFFIC_MODE_INTERVAL;
-//             break;
-//             case TRAFFIC_MODE_GAUSSIAN:
-//                 tenants[index].traffic_mode = TENANT_TRAFFIC_MODE_GAUSSIAN;
-//             break;            
-//             case TRAFFIC_MODE_ALL_NAUGHTY:
-//                 if(index>config.naughty_tenant_number)
-//                     tenants[index].traffic_mode = TENANT_TRAFFIC_MODE_INTERVAL;
-//                 else tenants[index].traffic_mode = TRAFFIC_MODE_ALL_NAUGHTY;
-//             break;
-//             default:
-//                 tenants[index].traffic_mode = TENANT_TRAFFIC_MODE_INTERVAL;
-//             break;
-//         }
-//     }
+    //     // Decide traffic mode
+    //     for(int index = 0; index < config.tenant_number; index++){
+    //         tenant t = (tenant)*(tenants+index);
+    //         switch(config.traffic_mode){
+    //             case TRAFFIC_MODE_INTERVAL:
+    //                 tenants[index].traffic_mode = TENANT_TRAFFIC_MODE_INTERVAL;
+    //             break;
+    //             case TRAFFIC_MODE_GAUSSIAN:
+    //                 tenants[index].traffic_mode = TENANT_TRAFFIC_MODE_GAUSSIAN;
+    //             break;
+    //             case TRAFFIC_MODE_ALL_NAUGHTY:
+    //                 if(index>config.naughty_tenant_number)
+    //                     tenants[index].traffic_mode = TENANT_TRAFFIC_MODE_INTERVAL;
+    //                 else tenants[index].traffic_mode = TRAFFIC_MODE_ALL_NAUGHTY;
+    //             break;
+    //             default:
+    //                 tenants[index].traffic_mode = TENANT_TRAFFIC_MODE_INTERVAL;
+    //             break;
+    //         }
+    //     }
 
-//     if(atoi(argv[1])==GENERATE_TRAFFIC)
-//          tenants_timestamp_translation_external(CONFIGURATION_PATH, config.tenant_number, STORED_PACKET_GENERATION_PATH, STORED_TIMESTAMP_PATH);
-    
-//     // const char *filename = "../data/test/timestamp/tenant_99.csv";
-//     // size_t size = 0;
-    
-//     // long long *timestamps = read_csv(filename, &size);
-//     // printf("Timestamps (%zu entries):\n", size);
-//     // for (size_t i = 0; i < size; i++) {
-//     //     printf("%ld\n", timestamps[i]);
-//     // }
+    //     if(atoi(argv[1])==GENERATE_TRAFFIC)
+    //          tenants_timestamp_translation_external(CONFIGURATION_PATH, config.tenant_number, STORED_PACKET_GENERATION_PATH, STORED_TIMESTAMP_PATH);
 
-//     // Free allocated memory
-//     free(timestamps);
+    //     // const char *filename = "../data/test/timestamp/tenant_99.csv";
+    //     // size_t size = 0;
 
-//     configuration config;
+    //     // long long *timestamps = read_csv(filename, &size);
+    //     // printf("Timestamps (%zu entries):\n", size);
+    //     // for (size_t i = 0; i < size; i++) {
+    //     //     printf("%ld\n", timestamps[i]);
+    //     // }
 
-//     if (ini_parse(CONFIGURATION_PATH, handler, &config) < 0) {
-//         printf("Can't load configuration\"%s\"\n", CONFIGURATION_PATH);
-//         return EXIT_FAILURE;
-//     }
+    //     // Free allocated memory
+    //     free(timestamps);
 
-//     int identifier = 0;
-//     tenant t = initialize_tenant(identifier, config.simulation_time);
-//     // for (long index=0;index<t.simulation_time;index++)
-//     //     if(index<=(t.simulation_time/2))
-//     //         *(t.generated_packets+index) = 159;
-//     //     else *(t.generated_packets+index) = 161;
-//     tenant_packet_generation(&t, config.mean, config.standard_deviation, config.naughty_mean, config.naughty_standard_deviation);
-//     translate_band_to_packets(&t, MBPS, config.packet_size);
+    //     configuration config;
 
-// #ifdef RECORD_PACKETS_GENERATION
-//     record_generated_packets(&t, STORED_PACKET_GENERATION_PATH);
-// #endif
+    //     if (ini_parse(CONFIGURATION_PATH, handler, &config) < 0) {
+    //         printf("Can't load configuration\"%s\"\n", CONFIGURATION_PATH);
+    //         return EXIT_FAILURE;
+    //     }
 
-//     timestamp_translation(&t);
-// #ifdef RECORD_TIMESTAMP
-//     record_timestamps(&t, STORED_TIMESTAMP_PATH);
-// #endif   
+    //     int identifier = 0;
+    //     tenant t = initialize_tenant(identifier, config.simulation_time);
+    //     // for (long index=0;index<t.simulation_time;index++)
+    //     //     if(index<=(t.simulation_time/2))
+    //     //         *(t.generated_packets+index) = 159;
+    //     //     else *(t.generated_packets+index) = 161;
+    //     tenant_packet_generation(&t, config.mean, config.standard_deviation, config.naughty_mean, config.naughty_standard_deviation);
+    //     translate_band_to_packets(&t, MBPS, config.packet_size);
+
+    // #ifdef RECORD_PACKETS_GENERATION
+    //     record_generated_packets(&t, STORED_PACKET_GENERATION_PATH);
+    // #endif
+
+    //     timestamp_translation(&t);
+    // #ifdef RECORD_TIMESTAMP
+    //     record_timestamps(&t, STORED_TIMESTAMP_PATH);
+    // #endif
 
     return EXIT_SUCCESS;
 }

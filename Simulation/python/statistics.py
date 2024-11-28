4b317f0972d1b8549567784f6da2307b7d9390f5
TsaiChuWang <wtsaichu@gmail.com>
2024-11-28 20:00:43 +0800

    [Version 2] Update the statistic information

--- Simulation/python/statistics.py
+++ Simulation/python/statistics.py
@@ -1,5 +1,9 @@
 import sys
 import pandas
+import shutil
+
+terminal_width = shutil.get_terminal_size().columns
+
 
 DATA_PATH = sys.argv[1]
 
@@ -7,19 +11,57 @@ dataframe = pandas.read_csv(DATA_PATH, header=None)
 data = dataframe.values.tolist()
 # print(dataframe)
 # print(len(dataframe))
-pure_loss = [float(tenant[4]) for tenant in data]
 
-print("Max  Loss(Pure)  : {:<12} %". format(max(pure_loss)))
-print("Min  Loss(Pure)  : {:<12} %". format(min(pure_loss)))
-print("Avg. Loss(Pure)  : {:<12.10} %". format(sum(pure_loss)/len(dataframe)))
+def calculateLoss_all(loss, index):
+  return (float(loss[index])*100.0)/(sum([float(loss[i]) for i in range(4)]))
 
-total_loss = [(float(tenant[2])+float(tenant[3]))/(float(tenant[0])+float(tenant[2])+float(tenant[3])) for tenant in data]
+def calculateLoss_pure(loss, index):
+  return (float(loss[index])*100.0)/(float(loss[0])+float(loss[2])+float(loss[3]))
 
+print('=' * terminal_width)
+print("Denominator with total packets")
+
+gcra1_loss = [calculateLoss_all(tenant, 1) for tenant in data]
+print("Max  Loss(GCRA1) : {:<12} %". format(max(gcra1_loss)))
+print("Min  Loss(GCRA1) : {:<12} %". format(min(gcra1_loss)))
+print("Avg. Loss(GCRA1) : {:<12.10} %". format(sum(gcra1_loss)/len(dataframe)))
+
+print('-' * terminal_width)
+
+gcra2_loss = [calculateLoss_all(tenant, 2) for tenant in data]
+print("Max  Loss(GCRA2) : {:<12} %". format(max(gcra2_loss)))
+print("Min  Loss(GCRA2) : {:<12} %". format(min(gcra2_loss)))
+print("Avg. Loss(GCRA2) : {:<12.10} %". format(sum(gcra2_loss)/len(dataframe)))
+print('-' * terminal_width)
+
+link_loss = [calculateLoss_all(tenant, 3) for tenant in data]
+print("Max  Loss(Link)  : {:<12} %". format(max(link_loss)))
+print("Min  Loss(Link)  : {:<12} %". format(min(link_loss)))
+print("Avg. Loss(Link)  : {:<12.10} %". format(sum(link_loss)/len(dataframe)))
+print('-' * terminal_width)
+
+total_loss = [calculateLoss_all(tenant, 1)+calculateLoss_all(tenant, 2)+calculateLoss_all(tenant, 3) for tenant in data]
 print("Max  Loss(Total) : {:<12} %". format(max(total_loss)))
 print("Min  Loss(Total) : {:<12} %". format(min(total_loss)))
 print("Avg. Loss(Total) : {:<12.10} %". format(sum(total_loss)/len(dataframe)))
 
-link_loss = [float(tenant[3])/(float(tenant[0])+float(tenant[1])+float(tenant[2])+float(tenant[3])) for tenant in data]
+print('=' * terminal_width)
+print("Denominator with packets in contract")
+
+gcra2_loss = [calculateLoss_pure(tenant, 2) for tenant in data]
+print("Max  Loss(GCRA2) : {:<12} %". format(max(gcra2_loss)))
+print("Min  Loss(GCRA2) : {:<12} %". format(min(gcra2_loss)))
+print("Avg. Loss(GCRA2) : {:<12.10} %". format(sum(gcra2_loss)/len(dataframe)))
+print('-' * terminal_width)
+
+link_loss = [calculateLoss_pure(tenant, 3) for tenant in data]
 print("Max  Loss(Link)  : {:<12} %". format(max(link_loss)))
 print("Min  Loss(Link)  : {:<12} %". format(min(link_loss)))
 print("Avg. Loss(Link)  : {:<12.10} %". format(sum(link_loss)/len(dataframe)))
+print('-' * terminal_width)
+
+total_loss = [calculateLoss_pure(tenant, 1)+calculateLoss_pure(tenant, 2)+calculateLoss_pure(tenant, 3) for tenant in data]
+print("Max  Loss(Total) : {:<12} %". format(max(total_loss)))
+print("Min  Loss(Total) : {:<12} %". format(min(total_loss)))
+print("Avg. Loss(Total) : {:<12.10} %". format(sum(total_loss)/len(dataframe)))
+print('=' * terminal_width)

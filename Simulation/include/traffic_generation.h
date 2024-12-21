cd371f1b8567768bc2c2f39e3c6bc821f6df8ca7
TsaiChuWang <wtsaichu@gmail.com>
2024-12-21 16:12:43 +0800

    [Version 2] Add bandwidth chart

--- Simulation/include/traffic_generation.h
+++ Simulation/include/traffic_generation.h
@@ -7,43 +7,51 @@
 
 #ifdef TRAFFIC_GENERATION_H
 
-typedef struct{
+typedef struct
+{
     long step_size;
     long grids_number;
 
     double generate_probability;
     double generate_probability_naughty;
-}traffic_generator;
+} traffic_generator;
 
-traffic_generator initializeTrafficGenerator(const configuration config){
+traffic_generator initializeTrafficGenerator(const configuration config)
+{
     traffic_generator generate;
 
-    generate.step_size = (long)((long)config.packet_size / (config.input_rate / ONE_SECOND_IN_NS));
-    generate.grids_number =  ONE_SECOND_IN_NS/config.packet_size;
+    double temp = 0;
+    temp = (double)ONE_SECOND_IN_NS / config.input_rate;
+    generate.step_size = (long)(config.packet_size * temp);
+    generate.grids_number = (long)((double)ONE_SECOND_IN_NS / generate.step_size);
 
     long unit = obtainUnit(config);
-    generate.generate_probability = (double)config.mean*unit/config.input_rate;
-    generate.generate_probability_naughty = (double)config.naughty_mean*unit/config.input_rate;
-    
-    return generate; 
+    generate.generate_probability = (double)config.mean * unit / config.input_rate;
+    generate.generate_probability_naughty = (double)config.naughty_mean * unit / config.input_rate;
+
+    return generate;
 }
 
-long obtain_grid_length(int simulation_time, int step_size){
+long obtain_grid_length(int simulation_time, int step_size)
+{
     return 1953125;
 }
 
-long obtain_grids_number(int packet_size){
-    return (long)ONE_SECOND_IN_NS/packet_size;
+long obtain_grids_number(int packet_size)
+{
+    return (long)ONE_SECOND_IN_NS / packet_size;
 }
 
-void showTrafficGenerator(const traffic_generator generate){
+void showTrafficGenerator(const traffic_generator generate)
+{
     printf("| step_size                    : %-ld\n", generate.step_size);
     printf("| grids_number                 : %-ld\n", generate.grids_number);
     printf("| generate_probability         : %-f\n", generate.generate_probability);
     printf("| generate_probability_naughty : %-f\n", generate.generate_probability_naughty);
 }
 
-int uniform_distribution(int random, double probability){
+int uniform_distribution(int random, double probability)
+{
     srand((unsigned int)time(NULL) + random);
     double factor = (rand() / (double)RAND_MAX);
     if (factor <= probability)
@@ -52,9 +60,11 @@ int uniform_distribution(int random, double probability){
         return PACKET_LABEL_NO_PACKET;
 }
 
-int* packet_generation_uniform(int random, double probability, int tenant_number){
-    int *packets = (int*)malloc(sizeof(int) * tenant_number);
-    for (long index = 0; index < tenant_number; index++){
+int *packet_generation_uniform(int random, double probability, int tenant_number)
+{
+    int *packets = (int *)malloc(sizeof(int) * tenant_number);
+    for (long index = 0; index < tenant_number; index++)
+    {
         *(packets + index) = uniform_distribution(random + index, probability);
         // printf("[%-10ld] : %-d\n", *(packets + index));
     }
@@ -63,22 +73,27 @@ int* packet_generation_uniform(int random, double probability, int tenant_number
 // int* packets = packet_generation_uniform(0, generator.generate_probability, config.tenant_number);
 // print_packets(packets, config.tenant_number);
 
-int *packet_generation_naughty(int random, double ratio_normal, double ratio_naughty, int tenant_number, int naughty_tenant_number){
+int *packet_generation_naughty(int random, double ratio_normal, double ratio_naughty, int tenant_number, int naughty_tenant_number)
+{
     int *packets = (int *)malloc(sizeof(int) * tenant_number);
-    for (int index = 0; index < tenant_number; index++){
-        if(index<naughty_tenant_number && GENERATE_BINARY)
+    for (int index = 0; index < tenant_number; index++)
+    {
+        if (index < naughty_tenant_number && GENERATE_BINARY)
             *(packets + index) = uniform_distribution(random + index, ratio_naughty);
-        else *(packets + index) = uniform_distribution(random + index, ratio_normal);
+        else
+            *(packets + index) = uniform_distribution(random + index, ratio_normal);
 
         // printf("[%-10ld] : %-d\n", *(packets + index));
     }
     return packets;
 }
 
-void print_packets(int *packets, long tenant_number){
+void print_packets(int *packets, long tenant_number)
+{
     long sum = 0;
     printf("sum = %ld, length = %ld %f\n", sum, tenant_number, (double)sum / tenant_number);
-    for (long index = 0; index < tenant_number; index++){
+    for (long index = 0; index < tenant_number; index++)
+    {
         if (*(packets + index) == PACKET_LABEL_ACCEPT)
             sum += 1;
 
@@ -89,7 +104,5 @@ void print_packets(int *packets, long tenant_number){
 }
 
 // void record_paclets(const char)
-
-
 
 #endif

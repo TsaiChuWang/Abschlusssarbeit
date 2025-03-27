301b9aad3ff5e11edb793fbe4986e9f9a08357b4
TsaiChuWang <wtsaichu@gmail.com>
2025-03-27 12:08:50 +0800

    Fullfill main comment

--- Simulation/python/report.py
+++ Simulation/python/report.py
@@ -1,42 +1,42 @@
-import matplotlib.pyplot as plt
-import pandas
-import numpy as np
-import sys
+# import matplotlib.pyplot as plt
+# import pandas
+# import numpy as np
+# import sys
 
-IMAGE_PATH = '../data/report.png'
-DATA_PATH = '../data/main/record_average_loss.csv'
-# DATA_PATH = '../data/naughty_1.csv'
+# IMAGE_PATH = '../data/report.png'
+# DATA_PATH = '../data/main/record_average_loss.csv'
+# # DATA_PATH = '../data/naughty_1.csv'
 
-# dataframe = pandas.read_csv(DATA_PATH, header=None)
-# data = dataframe.values.tolist()
+# # dataframe = pandas.read_csv(DATA_PATH, header=None)
+# # data = dataframe.values.tolist()
 
-dataframe = pandas.read_csv(DATA_PATH)
+# dataframe = pandas.read_csv(DATA_PATH)
 
-tau_original = dataframe["tau"].tolist()
-average_loss_original = dataframe["average_loss"].tolist()
-# state_r = dataframe["state_r"].tolist()
-
-
-# m = float(sys.argv[1])
+# tau_original = dataframe["tau"].tolist()
+# average_loss_original = dataframe["average_loss"].tolist()
+# # state_r = dataframe["state_r"].tolist()
 
 
-# tau = []
-# average_loss = []
-# for index, value in enumerate(state_r):
-#   if(value == m):
-#     tau.append(tau_original[index])
-#     average_loss.append(average_loss_original[index])
+# # m = float(sys.argv[1])
 
-plt.plot(tau_original , average_loss_original, linestyle='-', label='average_loss')
 
-# plt.plot(tau, average_loss, linestyle='-', label='average_loss')
-# plt.plot(tau, [0.1 for i in tau], linestyle='-', color = 'red', label='ε')
+# # tau = []
+# # average_loss = []
+# # for index, value in enumerate(state_r):
+# #   if(value == m):
+# #     tau.append(tau_original[index])
+# #     average_loss.append(average_loss_original[index])
 
-plt.title('Packet Loss with different τ (Naughty 140)')
-plt.xlabel('τ')
-plt.ylabel('Loss (%)')
-plt.legend()
-plt.grid(True)
+# plt.plot(tau_original , average_loss_original, linestyle='-', label='average_loss')
 
-plt.savefig(IMAGE_PATH)
-plt.cla()
+# # plt.plot(tau, average_loss, linestyle='-', label='average_loss')
+# # plt.plot(tau, [0.1 for i in tau], linestyle='-', color = 'red', label='ε')
+
+# plt.title('Packet Loss with different τ (Naughty 140)')
+# plt.xlabel('τ')
+# plt.ylabel('Loss (%)')
+# plt.legend()
+# plt.grid(True)
+
+# plt.savefig(IMAGE_PATH)
+# plt.cla()

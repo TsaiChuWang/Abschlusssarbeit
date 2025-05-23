16257d1b8070a778c842519dfe1ffe60136ece8a
TsaiChuWang <wtsaichu@gmail.com>
2025-05-23 16:27:23 +0800

    update for average loss chart

--- Simulation/python/average_loss.py
+++ Simulation/python/average_loss.py
@@ -3,34 +3,44 @@ import matplotlib.pyplot as plt
 import pandas
 import sys
 
-CONFIGURATION_PATH = '../configuration/main.ini'
+CONFIGURATION_PATH = sys.argv[2]
 config = configparser.ConfigParser()
 config.read(CONFIGURATION_PATH)
 
-IMAGE_PATH = config['simulation']['data_path']+'/images/record_average_loss.png'
+IMAGE_PATH = config['simulation']['data_path']+'/images/average_loss_{}_{}.png'
 DATA_PATH = config['simulation']['data_path']+'/record_average_loss.csv'
 
 dataframe = pandas.read_csv(DATA_PATH)
 
+name = sys.argv[1]
+
+print(list(dataframe))
 tau = dataframe['tau'].tolist()
-average_loss = dataframe['average_loss'].tolist()
+average_loss_pure = dataframe['average_loss_pure'].tolist()
+average_loss_all = dataframe['average_loss_all'].tolist()
 
-plt.plot(tau, average_loss, linestyle='-', label='average_loss')
-# plt.plot(tau, [0.1 for i in tau], linestyle='-', color = 'red', label='ε')
+plt.plot(tau, average_loss_pure, linestyle='-', label='average_loss(pure)')
+plt.title('Average Packet Loss with different τ ({})'.format(name))
 
-if(int(sys.argv[1])==0):
-    plt.title('Average Packet Loss with different τ (All Regular)')
-elif(int(sys.argv[1])==1):
-    plt.title('Average Packet Loss with different τ (Uniform Naughty {})'.format(int(config['traffic']['naughty_mean'])))
-elif(int(sys.argv[1])==2):
-    plt.title('Average Packet Loss with different τ (All Regular : Burst)')
-else:
-    plt.title('Average Packet Loss with different τ (All Regular)')
+# plt.plot(tau, [float(config['simulation']['error'])*100 for i in tau], linestyle='-', color = 'red', label='ε')
 
 plt.xlabel('τ')
 plt.ylabel('Loss (%)')
 plt.legend()
 plt.grid(True)
 
-plt.savefig(IMAGE_PATH)
+plt.savefig(IMAGE_PATH.format(name, 'pure'))
+plt.cla()
+
+plt.plot(tau, average_loss_pure, linestyle='-', label='average_loss(all)')
+plt.title('Average Packet Loss with different τ ({})'.format(name))
+
+# plt.plot(tau, [float(config['simulation']['error'])*100 for i in tau], linestyle='-', color = 'red', label='ε')
+
+plt.xlabel('τ')
+plt.ylabel('Loss (%)')
+plt.legend()
+plt.grid(True)
+
+plt.savefig(IMAGE_PATH.format(name, 'all'))
 plt.cla()
